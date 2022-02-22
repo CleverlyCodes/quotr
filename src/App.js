002d24b936cbcc90a1384278e238874ad99b049a
CleverlyCodes <cleverly.codes@gmail.com>
2022-02-23 03:06:34 +0800

    add basic form ui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {useState, useEffect} from 'react';
+import {Container, Row, Col, Form, Button} from 'react-bootstrap';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function addItem() {
+    setItems([...items, {
+      description: '',
+      amount: '',
+    }]);
+  };
+
+  function editItemDescription(index, value) {
+    let currentItems = [...items];
+    currentItems[index].description = value;
+    setItems(currentItems);
+  }
+
+  function editItemAmount(index, value) {
+    let currentItems = [...items];
+    currentItems[index].amount = value;
+    setItems(currentItems);
+  }
+
+  function generateQuote() {
+    console.log(items);  
+  }
+
+  useEffect(() => {}, [items])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header py-5"> 
+        <Container>
+          <Row className="mb-5">
+            <h1 classNames="mb-5">QUOTR</h1>
+          </Row>
+
+          <Row className="mb-4">
+            <Col>
+              <textarea placeholder="service description" className="w-100 px-3 py-1 fs-6"/>
+            </Col>
+          </Row>
+
+          {
+            items.map((item, index) => (
+              <Row className="mt-3">
+                <Col xs="8">
+                  <Form.Control value={item.description} onChange={(e) => editItemDescription(index, e.target.value)} type="email" placeholder="Description" />
+                </Col>
+    
+                <Col xs="4">
+                  <Form.Control value={item.amount} onChange={(e) => editItemAmount(index, e.target.value)} type="email" placeholder="Amount" />
+                </Col>
+              </Row>
+            ))
+          }
+
+          <Row className="mt-3">
+            <Col>
+              <Button className="w-100 btn-success fw-bold" onClick={addItem}>Add Item</Button>
+            </Col>
+          </Row>
+
+          <Row className="mt-5">
+            <Col></Col>
+
+            <Col>
+              <Button className="w-100 btn-primary fw-bold" onClick={generateQuote}>GeneRate</Button>
+            </Col>
+          </Row>
+        </Container>
       </header>
     </div>
   );
